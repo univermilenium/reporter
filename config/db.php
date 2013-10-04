9f65863d89a1de7f60029042eb655c748ef943aa
Moisés Rangel <mrangel@univermilenium>
2013-10-03 21:20:06 -0500

    conflicto

--- config/db.php
+++ config/db.php
@@ -12,21 +12,21 @@
 /**
  * database host, usually it's "127.0.0.1" or "localhost", some servers also need port info, like "127.0.0.1:8080"
  */
-define("DB_HOST", "127.0.0.1");
+define("DB_HOST", "");
 
 /**
  * name of the database. please note: database and database table are not the same thing!
  */
-define("DB_NAME", "reporter");
+define("DB_NAME", "onlineun_reporter");
 
 /**
  * user for your database. the user needs to have rights for SELECT, UPDATE, DELETE and INSERT.
  * by the way, it's bad style to use "root". In a real application you should create a database user
  * that fits your needs.
  */
-define("DB_USER", "root");
+define("DB_USER", "onlineun_report");
 
 /**
  * The password of the above user
  */
-define("DB_PASS", "mysql");
+define("DB_PASS", "Report01$");
